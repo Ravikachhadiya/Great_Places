7255931b53680a4d5ff0814e1c252aa839b09d31
Ravikachhadiya <ravikachhadiya@gmail.com>
2020-10-09 00:56:27 +0530

    session 13.15

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,7 +1,8 @@
-const GOOGLE_API_KEY = 'AIzaSyBg9yn5JtQgKRFbg6FCTy4ewbF24kRuAYI';
+const GOOGLE_API_KEY = 'AIzaSyDkFKbzvsJD2d0alNt6aEogvfFhFEW6fwc';
 
-class LocationHelper{
-  static String generateLocationPreviewImage({double latitude,  double longitude}) {
-return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {double latitude, double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
   }
 }
